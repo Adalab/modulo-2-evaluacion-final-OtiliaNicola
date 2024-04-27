fb28c168a8144b1e1c67da4b0d8c3e782ae1927c
OtiliaNicola <julio.guerrero@ezentis.com>
2024-04-27 10:30:30 +0200

    HTML structure and button events

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('>> Ready :)');
+//sacar los eventos click de los dos botones
+//en el de buscar obtener el texto
+
+const input = document.querySelector('.js_input_text');
+const btnSearch = document.querySelector('.js_search');
+const btnReset = document.querySelector('.js_reset');
+
+function handleSearch (event){
+    event.preventDefault();
+    //obtener el valor del input
+    const search = input.value;
+    console.log('El valor introducido es ' + search);
+}
+
+function handleReset(event){
+    event.preventDefault();
+    console.log('reset');
+}
+
+btnSearch.addEventListener('click', handleSearch);
+
+btnReset.addEventListener('click', handleReset);
